00b462b6b8d9555fa63aab45fb6e2a86e6eb25fe
Hassan TIMLI <timlihassan@hotmail.fr>
2020-11-16 21:15:06 +0100

    ajout fonction brute force segmentation fault

--- src/BiConProblem/BiConResolution.c
+++ src/BiConProblem/BiConResolution.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 
-bool est_solution(BiConGraph *graph){
+bool est_solution(BiConGraph *graph)
+{
 	for(int i=0; i< orderG(graph->graph);i++){
 		for(int j=0; j< orderG(graph->graph);j++){
 			if(isEdge(graph->graph,i,j)){
@@ -12,38 +13,76 @@ bool est_solution(BiConGraph *graph){
 				(!(graph->translators[i]) || !(graph->translators[j])))){
 					return false ;
 				}	
-			}
-	
+			}	
 		}
-	
-	
 	}
 	return true ;
 }
 
-bool BruteForceBiCon(BiConGraph *graph){
-	int count =1;
-	while(count < orderG(graph->graph)/2){
-		for(int cb=1;cb<count;cb++){ //TODO :pour_chaque_combinaison 
-			addTranslator(graph,cb);
-			for(int node=1; node<cb;node++){
-				addTranslator(graph,node);
-					if(!est_solution(graph)){
-						 resetTranslator(graph);
-					} 	
-					else {
-						return true ;
-					}					
-			}
-			if(!est_solution(graph)){
-				resetTranslator(graph);
-			} 		
-		}
-	}
-	
-    printf("Brute Force false \n");
-    return false;
+void avancer_index(int index,int n,int * indexTab,bool*vertexBitMap )
+{
+	if(indexTab[index] == n-index-1)	
+		avancer_index(index+1,n,indexTab,vertexBitMap);
+	vertexBitMap[indexTab[index]]=0;
+	indexTab[index]++;
+	vertexBitMap[indexTab[index]]=1;
+}	 			
 
+bool prochaine_combinaison(int cb_size,int n,int * indexTab,bool* vertexBitMap)
+{
+	bool end = true ;
+	for(int i=n-1 ; i>=n-cb_size ; i++){
+		if(!vertexBitMap[i]){
+			end = false ;
+			break ;
+		}
+	}	
+	if(end)
+		return false ;
+	avancer_index(0,n,indexTab,vertexBitMap);
+	return true ;
+}	 
+void ajouter_convertisseur(BiConGraph *graph,int n, bool*vertexBitMap)
+{
+	for(int j=0;j<n;j++){
+		if(vertexBitMap[j]){
+			addTranslator(graph,j);
+			return;
+		}	
+	}	
+}
+
+
+bool BruteForceBiCon(BiConGraph *G)
+{	
+	int n = orderG(G->graph);
+
+	bool vertexBitMap[n];
+	int indexTab[n/2+1];
+	
+	for(int i =0 ; i<n ; i++)
+		vertexBitMap[i]=false;
+	for(int i =0 ; i<n/2+1;i++)
+		indexTab[i]=0;
+		
+	for(int k=1 ; k<=n/2 ; k++){
+		for(int cb_size=1 ;cb_size<=k; k++){
+		
+			for(int i=0;i<cb_size;i++){
+				vertexBitMap[i]=1;
+				indexTab[i]=0;
+			}
+			do {
+				ajouter_convertisseur(G,n,vertexBitMap);
+				if(est_solution(G))
+					return true;
+				resetTranslator(G);		
+				} while(prochaine_combinaison(cb_size,n,indexTab,vertexBitMap));
+			for(int i =0 ; i<n ; i++)
+				vertexBitMap[i]=false;	
+		} 
+	}
+    return false;
 }
 bool GreedyBiCon(BiConGraph *graph){
     printf("Brute Force not implemented\n");
